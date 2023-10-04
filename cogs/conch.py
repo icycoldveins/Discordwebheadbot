ffa3204f08eccbec42aaaedb81f4ced2b68c5b2c
Kevin Wijaya <kevinwijaya306@gmail.com>
2023-10-04 03:08:15 -0400

    restructured directory for discord

---
+++ cogs/conch.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import random
+
+class Conch(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def conch(self, ctx, question: str):
+        answers = [
+           "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes, definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+        ]
+        answer = random.choice(answers)
+        await ctx.send(f"The Magic Conch says: {answer}")
+
+async def setup(bot):
+    await bot.add_cog(Conch(bot))
+
---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import config
+intents = discord.Intents.all()
+# Create a bot instance
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Load extension on bot startup
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name} - {bot.user.id}")
+    await bot.load_extension("cogs.conch")
+
+# Command to reload an extension (useful for development)
+@bot.command()
+async def reload(ctx, extension_name: str):
+    try:
+        bot.reload_extension(extension_name)
+        await ctx.send(f"Reloaded {extension_name}!")
+    except Exception as e:
+        await ctx.send(f"Error reloading {extension_name}: {e}")
+
+# Start the bot
+bot.run(config.BOT_TOKEN)
