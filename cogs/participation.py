75462d7a7796b1730afb41f2165aa525b9e6ed3f
Kevin Wijaya <kevinwijaya306@gmail.com>
2023-11-11 23:38:20 -0500

    added mongodb

--- cogs/participation.py
+++ cogs/participation.py
@@ -1,57 +1,51 @@
 import discord
 from discord.ext import commands
-import sqlite3
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class ParticipationCounter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.conn = sqlite3.connect('counter.db')
-        self.c = self.conn.cursor()
-        self.c.execute('''CREATE TABLE IF NOT EXISTS participation_counter
-                          (user_id INTEGER PRIMARY KEY, count INTEGER)''')
-        self.conn.commit()
+        self.client = MongoClient('MONGO_URL')
+        self.db = self.client['ParticipationCount']
+        self.collection = self.db['users']
 
     @commands.command()
     async def participation(self, ctx):
         user = ctx.author
-        self.c.execute(
-            "SELECT count FROM participation_counter WHERE user_id=?", (user.id,))
-        result = self.c.fetchone()
+        result = self.collection.find_one({'user_id': user.id})
         if result is None:
             count = 1
-            self.c.execute(
-                "INSERT INTO participation_counter VALUES (?, ?)", (user.id, count))
+            self.collection.insert_one({'user_id': user.id, 'count': count})
         else:
-            count = result[0] + 1
-            self.c.execute(
-                "UPDATE participation_counter SET count=? WHERE user_id=?", (count, user.id))
-        self.conn.commit()
+            count = result['count'] + 1
+            self.collection.update_one({'user_id': user.id}, {
+                                       '$set': {'count': count}})
         await ctx.send(f"{user.name} has participated {count} times this semester.")
 
     @commands.command()
     async def wipe(self, ctx):
-        self.c.execute("DELETE FROM participation_counter")
-        self.conn.commit()
+        self.collection.delete_many({})
         await ctx.send("Participation counter has been wiped.")
 
     @commands.command()
     async def undoparticipate(self, ctx):
         user = ctx.author
-        self.c.execute(
-            "SELECT count FROM participation_counter WHERE user_id=?", (user.id,))
-        result = self.c.fetchone()
+        result = self.collection.find_one({'user_id': user.id})
         if result is None:
             await ctx.send("You haven't participated yet.")
         else:
-            count = result[0] - 1
-            self.c.execute(
-                "UPDATE participation_counter SET count=? WHERE user_id=?", (count, user.id))
-            self.conn.commit()
+            count = result['count'] - 1
+            self.collection.update_one({'user_id': user.id}, {
+                                       '$set': {'count': count}})
             await ctx.send(f"{user.name} has participated {count} times.")
 
     def cog_unload(self):
-        self.conn.close()
+        self.client.close()
 
 
 async def setup(bot):
